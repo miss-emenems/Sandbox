aca136d2b03483e501ed5018ca4c9e4f9af35e9f
Martyna <martyna.machowiak@gmail.com>
2017-10-15 19:04:33 +0100

    font sizes em/rem test with bootstrap

--- head.php
+++ head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery-1.11.3.min.js"></script>
     <script src="js/scripts.js"></script>
